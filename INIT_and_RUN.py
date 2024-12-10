6645f828486212b738184bfe85fbcbb8a94025e2
locluklak <dtanloc.j@gmail.com>
2024-12-10 15:28:52 +0700

    correlated data

--- INIT_and_RUN.py
+++ INIT_and_RUN.py
@@ -8,27 +8,27 @@ def run(iter = 0):
 
     # print("Seed:",seed)
 
-    st = time.time()
+    # st = time.time()
     #___________________________________________________________
     ns = 30
     nt = 10
     p = 5
-    betat = 1
+    betat = 0
     true_beta_s = np.full((p,1), 2) #source's beta
     true_beta_t = np.full((p,1), betat) #target's beta
     k = 3 # k = -1 if choose AIC
     #___________________________________________________________
-    en = time.time()
+    # en = time.time()
     pvalue = pivot.pvalue_SI(seed, ns, nt, p, true_beta_s, true_beta_t, k)
 
     # pvalue = pivot_nonDA.pvalue_SI(seed, ns, p, true_beta_t)
 
     # Save pvalue into file
-    OCorPARA_FIXorAIC_FPRorTPR ='para_fixed_time'
+    OCorPARA_FIXorAIC_FPRorTPR ='para_correlated_fixed'
     # filename = f'Experiment/LstpBS_{OCorPARA_FIXorAIC_FPRorTPR}_{ns}_{p}.txt'
     filename = f'Experiment/LstBS_{OCorPARA_FIXorAIC_FPRorTPR}_{ns}_{p}_{betat}.txt'
     with open(filename, 'a') as f:
-        f.write(str(en - st)+ '\n')
+        f.write(str(pvalue)+ '\n')
     return pvalue
 
 if __name__ == "__main__":
--- Parallel_computing.py
+++ Parallel_computing.py
@@ -9,7 +9,7 @@ def main(st = 0):
     max_iteration = 120
     list_p_value = []
     # ssize = 50
-    alpha = 0.05 / (5*2**4) # / (3*comb(5,3))  
+    alpha = 0.05 #/ (5*2**4) # / (3*comb(5,3))  
     count = 0
     #print("core available: ", mpr.cpu_count())
     iter = range(max_iteration)
